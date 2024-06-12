3427f1a8b04f72fe034a6c66f0adf15a630d40fe
graminea <gabrielsagasdasilva@gmail.com>
2024-06-12 09:42:35 -0300

    ai papai

--- login.py
+++ login.py
@@ -42,33 +42,37 @@ class Login:
 
 
 class LoginApp(tk.Tk):
-    def __init__(self, on_login_success):
+    def __init__(self, login_system, on_login_success):
         super().__init__()
+        self.login_system = login_system
+        self.on_login_success = on_login_success
         self.title("Login")
         self.geometry("300x200")
-        self.login_manager = Login()
-        self.on_login_success = on_login_success
-        
+
         self.label_user = tk.Label(self, text="Usuário:")
-        self.label_user.pack()
+        self.label_user.grid(row=0, column=0, padx=5, pady=5)
         self.entry_user = tk.Entry(self)
-        self.entry_user.pack()
+        self.entry_user.grid(row=0, column=1, padx=5, pady=5)
 
         self.label_password = tk.Label(self, text="Senha:")
-        self.label_password.pack()
+        self.label_password.grid(row=1, column=0, padx=5, pady=5)
         self.entry_password = tk.Entry(self, show="*")
-        self.entry_password.pack()
+        self.entry_password.grid(row=1, column=1, padx=5, pady=5)
+
+        self.show_password = False
+        self.show_password_button = tk.Button(self, text='👁️', command=self.toggle_password)
+        self.show_password_button.grid(row=1, column=2, padx=5, pady=5)
 
         self.button_login = tk.Button(self, text="Login", command=self.perform_login)
-        self.button_login.pack()
+        self.button_login.grid(row=2, column=0, columnspan=3, pady=5)
 
         self.button_register = tk.Button(self, text="Registrar", command=self.perform_register)
-        self.button_register.pack()
+        self.button_register.grid(row=3, column=0, columnspan=3, pady=5)
 
     def perform_login(self):
         user = self.entry_user.get()
         password = self.entry_password.get()
-        if self.login_manager.login(user, password):
+        if self.login_system.login(user, password):
             messagebox.showinfo("Sucesso", "Login efetuado com sucesso!")
             self.destroy()
             self.on_login_success(user)
@@ -78,13 +82,22 @@ class LoginApp(tk.Tk):
     def perform_register(self):
         user = self.entry_user.get()
         password = self.entry_password.get()
-        self.login_manager.register(user, password)
+        self.login_system.register(user, password)
         messagebox.showinfo("Sucesso", "Usuário registrado com sucesso! Realize o login.")
+
+    def toggle_password(self):
+        if self.show_password:
+            self.entry_password.config(show="*")
+        else:
+            self.entry_password.config(show="")
+        self.show_password = not self.show_password
+
 
 if __name__ == '__main__':
     def on_login_success(user):
         app = WeatherApp(user)
         app.mainloop()
 
-    login_app = LoginApp(on_login_success)
+    login_system = Login()
+    login_app = LoginApp(login_system, on_login_success)
     login_app.mainloop()
